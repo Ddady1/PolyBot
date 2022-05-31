a61d1d3d60e3c9f22e280d32171a437f0da28436
Dady <ddady1@gmail.com>
2022-05-31 18:46:27 +0300

    YouTube Try

--- bot.py
+++ bot.py
@@ -35,7 +35,6 @@ class Bot:
 class QuoteBot(Bot):
     def _message_handler(self, update, context):
         to_quote = True
-
         if update.message.text == 'Don\'t quote me please':
             to_quote = False
             self.send_text(update, f'QuotBot message: {update.message.text}', quote=to_quote)
@@ -43,13 +42,15 @@ class QuoteBot(Bot):
             self.send_text(update, f'Original message: {update.message.text}')
 
 class YoutubeBot(Bot):
-    pass
+    def _message_handler(self, update, context):
 
+        self.send_video(update, context, file_path='C:/Users/Dady/PycharmProjects/PolyBot/')
 
 if __name__ == '__main__':
     with open('.telegramToken') as f:
         _token = f.read()
 
-    my_bot = QuoteBot(_token)
+    #my_bot = QuoteBot(_token)
+    my_bot = YoutubeBot(_token)
     my_bot.start()
 
