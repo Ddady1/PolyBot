9814623266a10d60d1773f2b89cf23da0376c642
Dady <ddady1@gmail.com>
2023-01-12 12:00:14 +0200

    main of worker

--- services/worker/app.py
+++ services/worker/app.py
@@ -46,6 +46,9 @@ if __name__ == '__main__':
     with open('common/config.json') as f:
         config = json.load(f)
 
+    with open('common/s3_file.txt', "a") as fileS3:
+        fileS3.write('main of worker\n')
+
     sqs = boto3.resource('sqs', region_name=config.get('aws_region'))
     queue = sqs.get_queue_by_name(QueueName=config.get('bot_to_worker_queue_name'))
 
